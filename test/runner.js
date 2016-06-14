c5502b5ed847529fbd6d3405c7bd171cca7d8ef4
Jake Fried <jzfisgreat@gmail.com>
2016-06-14 18:34:23 -0400

    Modifications according to PR feedback.

--- test/runner.js
+++ test/runner.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 'use strict'; // eslint-disable-line strict
-var files;
+var testFiles;
 
 require( 'babel-register' );
 
@@ -12,14 +12,8 @@ const debug = require( 'debug' )( 'test-runner' ),
 	Mocha = require( 'mocha' ),
 	path = require( 'path' ),
 	program = require( 'commander' ),
+	watchFile = require( 'fs' ).watchFile,
 	chalk = require( 'chalk' );
-
-/**
- * Internal dependencies
- */
-const boot = require( './boot-test' ),
-	utils = require( './utils.js' ),
-	setup = require( './setup' );
 
 program
 	.usage( '[options] [files]' )
@@ -33,18 +27,30 @@ program.name = 'runner';
 
 program.parse( process.argv );
 
-let getMocha = function() {
-	let mocha = new Mocha( {
+const getMocha = function() {
+	/* These internal dependencies are not at the top of the file
+	 * because we need to re-require them every time we invalidate the cache.
+	 *
+	 * We invalidate the cache because Mocha has trouble running a test-suite
+	 * more than once:  https://github.com/mochajs/mocha/issues/995
+	 */
+	Object.keys( require.cache ).forEach( ( key ) => {
+		delete require.cache[ key ];
+	} );
+	const boot = require( './boot-test' );
+	const setup = require( './setup' );
+
+	const mocha = new Mocha( {
 		ui: 'bdd',
 		reporer: program.reporter
 	} );
 
+	mocha.suite.beforeAll( boot.before );
+	mocha.suite.afterAll( boot.after );
+
 	if ( program.grep ) {
 		mocha.grep( new RegExp( program.grep ) );
 	}
-
-	mocha.suite.beforeAll( boot.before );
-	mocha.suite.afterAll( boot.after );
 
 	if ( process.env.CIRCLECI ) {
 		debug( 'Hello Circle!' );
@@ -53,17 +59,13 @@ let getMocha = function() {
 		mocha.suite.timeout( 10000 );
 	}
 
-	let testSuiteLoaderPath = path.join( __dirname, 'load-suite.js' );
-	// Mocha requires that you invalidate the cache in order to run a test-suite
-	// more than once: https://github.com/mochajs/mocha/issues/995
-	delete require.cache[ testSuiteLoaderPath ];
-	mocha.addFile( testSuiteLoaderPath );
-
+	testFiles.forEach( setup.addFile );
+	mocha.addFile( path.join( __dirname, 'load-suite.js' ) );
 	return mocha;
 };
 
-files = program.args.length ? program.args : [ process.env.TEST_ROOT ];
-files = files.reduce( ( memo, filePath ) => {
+testFiles = program.args.length ? program.args : [ process.env.TEST_ROOT ];
+testFiles = testFiles.reduce( ( memo, filePath ) => {
 	// Validate test root matches specified file paths
 	if ( ! filePath.startsWith( process.env.TEST_ROOT ) ) {
 		console.warn(
@@ -92,49 +94,42 @@ files = files.reduce( ( memo, filePath ) => {
 }, [] );
 
 if ( program.nodeTotal > 1 ) {
-	files = files.filter( ( file, index ) => {
+	testFiles = testFiles.filter( ( file, index ) => {
 		return index % program.nodeTotal === program.nodeIndex;
 	} );
 }
 
-files.forEach( setup.addFile );
+let runner = null;
+const runMocha = () => {
+	console.log( chalk.green( 'Running tests at: ' ),
+		chalk.yellow( new Date() ) );
+
+	runner = getMocha().run( () => {
+		runner = null;
+	} );
+};
 
 if ( program.watch ) {
 	console.log( chalk.green( 'Watch mode enabled' ) );
 
-	let runAgain = false;
-	let runner = null;
-
-	let rerun = () => {
-		console.log( chalk.green( 'Running tests at: ' ),
-									chalk.yellow( new Date( ) ) );
-		runAgain = false;
-
-		runner = getMocha().run( function( ) {
-			runner = null;
-			if ( runAgain ) {
-				rerun();
+	const watchFiles = glob.sync( '**/*.@(js|jsx)', { ignore: 'node_modules/**' } );
+	const options = { interval: 100 };
+	watchFiles.forEach( ( file ) => {
+		watchFile( file, options, () => {
+			if ( runner ) {
+				runner.abort();
 			}
-		} );
-	};
-
-	// start the first test-run independently of watch
-	rerun();
-
-	let watchFiles = utils.files( process.cwd( ), [ 'js', 'jsx' ] );
-	utils.watch( watchFiles, () => {
-		runAgain = true;
-		if ( runner ) {
-			runner.abort();
-		} else {
-			rerun();
-		}
-	} );
-} else {
-	getMocha().run( function( failures ) {
-		process.on( 'exit', function() {
-			process.exit( failures ); //eslint-disable-line no-process-exit
+			// aborting is not instant, so wait for it to finish
+			// then call run the test-suite again
+			let abortedCheck = setInterval( () => {
+				if ( runner == null ) {
+					clearInterval( abortedCheck );
+					runMocha();
+				}
+			}, 25 );
 		} );
 	} );
 }
+
+runMocha();
 
