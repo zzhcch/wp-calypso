7852d32b58c7db222cca31a80412f20f32086405
Yoav Farhi <yoav@farhi.org>
2016-07-27 23:53:51 +0300

    initial code to apply locale variants. First application: sr_latin

--- client/boot/index.js
+++ client/boot/index.js
@@ -28,6 +28,7 @@ var config = require( 'config' ),
 	abtest = abtestModule.abtest,
 	getSavedVariations = abtestModule.getSavedVariations,
 	switchLocale = require( 'lib/i18n-utils/switch-locale' ),
+	localeVariants = require( 'lib/i18n-utils/locale-variants' ),
 	analytics = require( 'lib/analytics' ),
 	route = require( 'lib/route' ),
 	normalize = require( 'lib/route/normalize' ),
@@ -159,6 +160,7 @@ function boot() {
 		localeSlug = user.get().localeSlug;
 		if ( localeSlug ) {
 			switchLocale( localeSlug );
+			localeVariants.init();
 		}
 	}
 	// Set the locale for the current user
@@ -166,6 +168,7 @@ function boot() {
 		localeSlug = user.get().localeSlug;
 		if ( localeSlug ) {
 			switchLocale( localeSlug );
+			localeVariants.init();
 		}
 	} );
 
