6bc1b647f3db43b2b1935153b14e4cf0bd43274a
Yoav Farhi <yoav@farhi.org>
2016-07-28 12:14:52 +0300

    Add new translations for formal german

--- client/lib/i18n-utils/locale-variants.js
+++ client/lib/i18n-utils/locale-variants.js
@@ -2,6 +2,7 @@
  * External dependencies
  */
 import debugFactory from 'debug';
+import request from 'superagent';
 import to_latin from 'cyrillic-to-latin';
 import i18n from 'i18n-calypso';
 
@@ -9,6 +10,7 @@ import i18n from 'i18n-calypso';
  * Internal dependencies
  */
 import userSettings from 'lib/user-settings';
+import i18nUtils from 'lib/i18n-utils';
 
 const debug = debugFactory( 'calypso:i18n' );
 
@@ -16,6 +18,10 @@ const localeVariants = {
 	init() {
 		const localeVariant = userSettings.getSetting( 'locale_variant' );
 		switch ( localeVariant ) {
+			case 'de_formal':
+				debug( 'Adding new translations for ' + localeVariant );
+				this.addTranslations( localeVariant );
+				break;
 			case 'sr_latin':
 				debug( 'Applying mods for ' + localeVariant );
 				i18n.registerTranslateHook( ( translation ) => {
@@ -41,6 +47,16 @@ const localeVariants = {
 		}
 
 		return translation;
+	},
+
+	addTranslations( localeVariantSlug ) {
+		request.get( i18nUtils.languageFileUrl( localeVariantSlug ) ).end( function( error, response ) {
+			if ( error ) {
+				debug( 'Encountered an error loading locale file for ' + localeVariantSlug );
+				return;
+			}
+			i18n.addTranslations( response.body );
+		} );
 	}
 };
 
