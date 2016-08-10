66a7e0b9b68262084f1eaac76c38edc56757d9b4
Copons <copons@users.noreply.github.com>
2016-08-10 16:11:51 +0200

    Integrate copy logic to Post Editor routing
    Moved the copy logic into a separate script in order to integrate it
    into the Post Editor routing, using a new page.js callback.
    The function now works fine, but definitely needs a review from someone
    that knows more than me about the best way of passing data between pages
    and of (pre-)loading the editor.

--- client/post-editor/controller.js
+++ client/post-editor/controller.js
@@ -26,6 +26,7 @@ import { getSelectedSiteId } from 'state/ui/selectors';
 import { getEditorPostId, getEditorPath } from 'state/ui/editor/selectors';
 import { editPost } from 'state/posts/actions';
 import wpcom from 'lib/wp';
+import { startEditingPostCopy } from 'post-editor/editor-copy-post';
 
 function getPostID( context ) {
 	if ( ! context.params.post || 'new' === context.params.post ) {
@@ -229,6 +230,30 @@ module.exports = {
 		const redirectWithParams = [ redirectPath, queryString ].join( '?' );
 
 		page.redirect( redirectWithParams );
+		return false;
+	},
+
+	copyPost: function( context, next ) {
+		if ( ! context.query.copy ) {
+			// not copyPost, early return
+			return next();
+		}
+
+		const postId = getPostID( context );
+
+		function startEditingOnSiteSelected() {
+			const siteId = getSelectedSiteId( context.store.getState() );
+			if ( siteId ) {
+				startEditingPostCopy( context, siteId, postId ).then( () => {
+					renderEditor( context, 'post' );
+				} );
+			} else {
+				sites.once( 'change', startEditingOnSiteSelected );
+			}
+		}
+
+		startEditingOnSiteSelected();
+
 		return false;
 	},
 
