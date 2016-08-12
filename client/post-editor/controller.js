ecec1aa57eed54a123a5e404e1d96c4e106a7287
Copons <copons@users.noreply.github.com>
2016-08-12 12:43:31 +0200

    Fix rebase conflicts and improve readability
    The two dispatches were unnecessarily unclear, so I simply moved the
    logic away, also streamlining the copied post ID usage.

--- client/post-editor/controller.js
+++ client/post-editor/controller.js
@@ -26,7 +26,6 @@ import { getSelectedSiteId } from 'state/ui/selectors';
 import { getEditorPostId, getEditorPath } from 'state/ui/editor/selectors';
 import { editPost } from 'state/posts/actions';
 import wpcom from 'lib/wp';
-import { startEditingPostCopy } from 'post-editor/editor-copy-post';
 
 function getPostID( context ) {
 	if ( ! context.params.post || 'new' === context.params.post ) {
@@ -140,11 +139,11 @@ module.exports = {
 	post: function( context ) {
 		const postType = determinePostType( context );
 		const postID = getPostID( context );
+		const copyId = context.query.copy || false;
 
 		function startEditing( siteId ) {
-			const isCopy = context.query.copy ? true : false;
-			context.store.dispatch( setEditorPostId( isCopy ? null : postID ) );
-			context.store.dispatch( editPost( siteId, ( isCopy ? null : postID ), { type: postType } ) );
+			context.store.dispatch( setEditorPostId( postID ) );
+			context.store.dispatch( editPost( siteId, postID, { type: postType } ) );
 
 			if ( maybeRedirect( context ) ) {
 				return;
@@ -160,12 +159,12 @@ module.exports = {
 			// We have everything we need to start loading the post for editing,
 			// so kick it off here to minimize time spent waiting for it to load
 			// in the view components
-			if ( postID && ! isCopy ) {
+			if ( postID && ! copyId ) {
 				// TODO: REDUX - remove flux actions when whole post-editor is reduxified
 				actions.startEditingExisting( siteId, postID );
 				analytics.pageView.record( '/' + postType + '/:blogid/:postid', gaTitle + ' > Edit' );
-			} else if ( isCopy ) {
-				startEditingPostCopy( siteId, context.query.copy );
+			} else if ( copyId ) {
+				startEditingPostCopy( siteId, copyId );
 				analytics.pageView.record( '/' + postType, gaTitle + ' > New' );
 			} else {
 				let postOptions = { type: postType };
@@ -230,28 +229,6 @@ module.exports = {
 		const redirectWithParams = [ redirectPath, queryString ].join( '?' );
 
 		page.redirect( redirectWithParams );
-		return false;
-	},
-
-	copyPost: function( context, next ) {
-		if ( ! context.query.copy ) {
-			// not copyPost, early return
-			return next();
-		}
-
-		const postId = getPostID( context );
-
-		function startEditingOnSiteSelected() {
-			const siteId = getSelectedSiteId( context.store.getState() );
-			if ( siteId ) {
-				startEditingPostCopy( context, siteId, postId );
-			} else {
-				sites.once( 'change', startEditingOnSiteSelected );
-			}
-		}
-
-		startEditingOnSiteSelected();
-		renderEditor( context, 'post' );
 		return false;
 	},
 
