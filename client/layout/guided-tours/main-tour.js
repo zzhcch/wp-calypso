b44a463594ad9ca60c63d269c4622fd5e39efed1
Leif Singer <git@singer.sh>
2016-08-12 13:46:53 +0200

    some cleanup

--- client/layout/guided-tours/main-tour.js
+++ client/layout/guided-tours/main-tour.js
@@ -1,3 +1,5 @@
+/** @ssr-ready **/
+
 import React from 'react';
 import { translate } from 'i18n-calypso';
 import { overEvery as and } from 'lodash';
@@ -211,7 +213,7 @@ export const MainTour = makeTour(
 					{ translate( "We're all done!" ) }
 				</Quit>
 			</div>
-			<Link href="https://lean.wordpress.com">
+			<Link href="https://learn.wordpress.com">
 				{ translate( 'Learn more about WordPress.com' ) }
 			</Link>
 		</Step>
