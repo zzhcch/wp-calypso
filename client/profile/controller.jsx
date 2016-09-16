bef7bed5ad53a49a8087919efddfa24ceaf1f4eb
Ben Lowery <ben.lowery@automattic.com>
2016-09-16 08:31:29 -0700

    add in some elements with faked data

--- client/profile/controller.jsx
+++ client/profile/controller.jsx
@@ -8,11 +8,26 @@ import ReactDom from 'react-dom';
  * Internal Dependencies
  */
 import Main from 'components/main';
+import Gravatar from 'components/gravatar';
 
 function Profile( { userId } ) {
+	const fakeUser = {
+		name: 'Len Bowery',
+		username: userId,
+		avatar_URL: null
+	};
 	return (
 		<Main>
-			<div>{ userId }</div>
+			<Gravatar user={ fakeUser } size={ 96 } />
+			<h1 className="profile__name">{ fakeUser.name }</h1>
+			<h2 className="profile__username">{ fakeUser.username }</h2>
+			<section>
+				<h2>Posts</h2>
+				<ol>
+					<li>post</li>
+					<li>post</li>
+				</ol>
+			</section>
 		</Main>
 	);
 }
