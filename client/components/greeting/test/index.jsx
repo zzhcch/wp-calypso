cc741ec15289fc57464b3cea6e14198cd63668e9
Ben Lowery <ben.lowery@automattic.com>
2016-09-17 11:33:29 -0700

    break one

--- client/components/greeting/test/index.jsx
+++ client/components/greeting/test/index.jsx
@@ -3,23 +3,43 @@
  */
 import React from 'react';
 import { expect } from 'chai';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 
 /**
  * Internal Dependencies
  */
 import { Greeting } from '../';
 import Gravatar from 'components/gravatar';
+import useFakeDom from 'test/helpers/use-fake-dom';
 
 function fakeTranslate( str ) {
 	return str;
 }
 
 describe( 'Greeting', () => {
-	it( 'should render nicely', () => {
+	useFakeDom();
+
+	it( 'should render empty nicely', () => {
 		const wrapper = shallow( <Greeting translate={ fakeTranslate } /> );
 		expect( wrapper.find( '.greeting__name' ) ).to.have.text( 'Howdy' );
 		expect( wrapper.find( '.greeting__name-length' ) ).to.have.text( '' );
+		expect( wrapper.find( Gravatar ) ).to.have.length( 1 );
+	} );
+
+	it( 'should render a name nicely', () => {
+		let callingArgs;
+		function fakeTranslateThatRememberArgs( str, options ) {
+			callingArgs = options.args;
+			return str;
+		}
+		const wrapper = mount(
+			<Greeting
+				translate={ fakeTranslateThatRememberArgs }
+				name="ben"
+			/> );
+		expect( wrapper.find( '.greeting__name' ) ).to.have.text( 'Hello %s' );
+		expect( callingArgs[ 0 ] ).to.equal( 'ben' );
+		expect( wrapper.find( '.greeting__name-length' ) ).to.have.text( '3' );
 		expect( wrapper.find( Gravatar ) ).to.have.length( 1 );
 	} );
 } );
