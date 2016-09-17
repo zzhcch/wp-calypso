8602bc16937d502c05fd35903e792cafb1f1954f
Davor Altman <davor.altman@gmail.com>
2016-09-17 16:49:25 -0700

    add Power Puff Girls

--- client/reader/post-header/index.jsx
+++ client/reader/post-header/index.jsx
@@ -10,6 +10,18 @@ import noop from 'lodash/noop';
 import Site from 'blocks/site';
 import FollowButton from 'reader/follow-button';
 
+
+
+function returnRandom () {
+
+var ppg = ["http://calypso.localhost:3000/assets/ppp.jpg", "http://calypso.localhost:3000/assets/ppp1.jpg", "http://calypso.localhost:3000/assets/ppp2.jpg"];
+
+var ppgNumber = Math.floor(Math.random() * 3);
+
+return ppg[ppgNumber]
+
+}
+
 const PostHeader = ( { site, siteUrl, showFollow, onSiteSelect, onSiteClick } ) => (
 	<div className="reader__post-header">
 		{ showFollow ? <FollowButton siteUrl={ siteUrl } /> : null }
@@ -17,6 +29,7 @@ const PostHeader = ( { site, siteUrl, showFollow, onSiteSelect, onSiteClick } ) 
 			href={ siteUrl }
 			onSelect={ onSiteSelect }
 			onClick={ onSiteClick } />
+			<img src={returnRandom()} />
 	</div>
 );
 
