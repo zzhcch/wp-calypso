67241d07de177cbc28006fac9dd02cc5d00963c0
Valerie <valerie@automattic.com>
2016-10-04 17:50:44 +0300

    Allow uploading of user-selected image to Gravatar

--- client/me/gravatar-updater/index.jsx
+++ client/me/gravatar-updater/index.jsx
@@ -12,8 +12,53 @@ import FilePicker from 'components/file-picker';
 import FormLabel from 'components/forms/form-label';
 import { getCurrentUser } from 'state/current-user/selectors';
 import Gravatar from 'components/gravatar';
+import * as OAuthToken from 'lib/oauth-token';
 
 export class GravatarUpdater extends React.Component {
+	handleOnPick( files ) {
+		console.log( 'you picked',  JSON.stringify( files[0].name ) );
+
+		// check for bearerToken from desktop app
+		let bearerToken = OAuthToken.getToken();
+
+		if ( ! bearerToken ) {
+			bearerToken = localStorage.getItem( 'bearerToken' );
+		}
+
+		// send gravatar request
+		if ( bearerToken ) {
+			console.log( 'Got the bearerToken, sending request' );
+
+			const data = new FormData();
+			data.append( 'filedata', files[0] );
+			data.append( 'account', this.props.user.email );
+
+			const obj = {
+				method: 'POST',
+				headers: {
+					'Authorization': 'Bearer ' + bearerToken,
+					'Accept-Language': '*'
+				},
+				body: data,
+			};
+
+		fetch( 'https://api.gravatar.com/v1/upload-image', obj )
+			.then( function ( result ) {
+				if ( result.ok ) {
+					return result.json();
+				}
+			} )
+			.then( function ( result ) {
+				console.log( 'fetch result', result );
+			} )
+			.catch( function ( error ) {
+				console.log( 'error', error );
+			} );
+		} else {
+			console.log( 'Oops - no bearer token.' );
+		}
+	}
+
 	render() {
 		return (
 			<div>
@@ -26,7 +71,7 @@ export class GravatarUpdater extends React.Component {
 				<p>
 					To change, select an image or drag and drop a picture from your computer.
 				</p>
-				<FilePicker accept="image/*">
+				<FilePicker accept="image/*" onPick={ this.handleOnPick.bind( this ) }>
 					<Button>Select Image</Button>
 				</FilePicker>
 			</div>
