f60e5c1e01605a342bb3211550b6d431b39daa28
Andrew Duthie <andrew@andrewduthie.com>
2016-10-07 21:04:56 -0400

    Framework: Use original raw request path in require

--- server/bundler/loaders/async-component-loader.js
+++ server/bundler/loaders/async-component-loader.js
@@ -1,6 +1,10 @@
 module.exports = function() {
 	this.cacheable();
 
+	// Use the original "raw" request path, not the resolved absolute
+	const rawRequest = this._module.rawRequest;
+	const resource = rawRequest.substr( rawRequest.lastIndexOf( '!' ) + 1 );
+
 	return `
 /**
  * External dependencies
@@ -13,6 +17,6 @@ import React from 'react';
 import AsyncLoad from 'components/async-load';
 
 export default function( props ) {
-	return <AsyncLoad require="${ this.resourcePath }" { ...props } />;
+	return <AsyncLoad require="${ resource }" { ...props } />;
 }`;
 };
