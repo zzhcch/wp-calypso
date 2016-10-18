b65a8eff903c939c7c4c3b1ed2505ffc6eeb0dc2
Riad Benguella <benguella@gmail.com>
2016-10-18 15:39:38 +0100

    Editor: Reduxify The EditorTitle component
     * Drop sitesList usage
     * Drop PostEditStore calls

--- client/post-editor/post-editor.jsx
+++ client/post-editor/post-editor.jsx
@@ -20,7 +20,7 @@ const actions = require( 'lib/posts/actions' ),
 	EditorDrawer = require( 'post-editor/editor-drawer' ),
 	FeaturedImage = require( 'post-editor/editor-featured-image' ),
 	EditorGroundControl = require( 'post-editor/editor-ground-control' ),
-	EditorTitleContainer = require( 'post-editor/editor-title/container' ),
+	EditorTitle = require( 'post-editor/editor-title' ),
 	EditorPageSlug = require( 'post-editor/editor-page-slug' ),
 	protectForm = require( 'lib/mixins/protect-form' ),
 	TinyMCE = require( 'components/tinymce' ),
@@ -214,7 +214,7 @@ const PostEditor = React.createClass( {
 								post={ this.state.post }
 								maxWidth={ 1462 } />
 							<div className="editor__header">
-								<EditorTitleContainer
+								<EditorTitle
 									onChange={ this.debouncedAutosave }
 									tabIndex={ 1 } />
 								{ this.state.post && isPage && site
