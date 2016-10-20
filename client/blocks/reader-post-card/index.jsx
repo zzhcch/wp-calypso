21b19a5c01792db28778034269ee1cc1a989f909
bluefuton <chris@bluefuton.com>
2016-10-21 12:24:54 +1300

    Add follow button to post card

--- client/blocks/reader-post-card/index.jsx
+++ client/blocks/reader-post-card/index.jsx
@@ -15,6 +15,7 @@ import DisplayTypes from 'state/reader/posts/display-types';
 import ReaderPostActions from 'blocks/reader-post-actions';
 import * as stats from 'reader/stats';
 import PostByline from './byline';
+import FollowButton from 'reader/follow-button';
 
 function FeaturedImage( { image, href } ) {
 	return (
@@ -32,12 +33,14 @@ export default class RefreshPostCard extends React.Component {
 		site: PropTypes.object,
 		feed: PropTypes.object,
 		onClick: PropTypes.func,
-		onCommentClick: PropTypes.func
+		onCommentClick: PropTypes.func,
+		showFollow: PropTypes.bool
 	};
 
 	static defaultProps = {
 		onClick: noop,
-		onCommentClick: noop
+		onCommentClick: noop,
+		showFollow: true
 	};
 
 	propagateCardClick = () => {
@@ -92,7 +95,7 @@ export default class RefreshPostCard extends React.Component {
 	}
 
 	render() {
-		const { post, site, feed, onCommentClick } = this.props;
+		const { post, site, feed, onCommentClick, showFollow } = this.props;
 		const featuredImage = post.canonical_image;
 		const isPhotoOnly = post.display_type & DisplayTypes.PHOTO_ONLY;
 		const title = truncate( post.title, {
@@ -104,9 +107,15 @@ export default class RefreshPostCard extends React.Component {
 			'is-photo': isPhotoOnly
 		} );
 
+		let followUrl;
+		if ( showFollow ) {
+			followUrl = feed ? feed.feed_URL : post.site_URL;
+		}
+
 		return (
 			<Card className={ classes } onClick={ this.handleCardClick }>
 				<PostByline post={ post } site={ site } feed={ feed } />
+				{ showFollow && <FollowButton siteUrl={ followUrl } /> }
 				<div className="reader-post-card__post">
 					{ featuredImage && <FeaturedImage image={ featuredImage } href={ post.URL } /> }
 					<div className="reader-post-card__post-details">
