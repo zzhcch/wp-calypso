1d49d3d7507803d46059d7407b02f53ad0d816d2
Kirk Wight <kwight@kwight.ca>
2016-10-24 15:07:06 -0700

    Update the Domains step to the current `master`.

--- client/signup/steps/domains/index.jsx
+++ client/signup/steps/domains/index.jsx
@@ -2,6 +2,7 @@
  * External dependencies
  */
 var React = require( 'react' ),
+	{ connect } = require( 'react-redux' ),
 	defer = require( 'lodash/defer' ),
 	page = require( 'page' ),
 	i18n = require( 'i18n-calypso' );
@@ -13,16 +14,19 @@ var StepWrapper = require( 'signup/step-wrapper' ),
 	productsList = require( 'lib/products-list' )(),
 	cartItems = require( 'lib/cart-values' ).cartItems,
 	SignupActions = require( 'lib/signup/actions' ),
-	MapDomain = require( 'components/domains/map-domain' ),
+	MapDomainStep = require( 'components/domains/map-domain-step' ),
 	RegisterDomainStep = require( 'components/domains/register-domain-step' ),
 	GoogleApps = require( 'components/upgrades/google-apps' ),
 	Notice = require( 'components/notice' ),
-	abtest = require( 'lib/abtest' ).abtest,
+	{ getCurrentUser, currentUserHasFlag } = require( 'state/current-user/selectors' ),
+	{ DOMAINS_WITH_PLANS_ONLY } = require( 'state/current-user/constants' ),
+	analyticsMixin = require( 'lib/mixins/analytics' ),
 	signupUtils = require( 'signup/utils' );
 
-module.exports = React.createClass( {
-	displayName: 'DomainsStep',
+const registerDomainAnalytics = analyticsMixin( 'registerDomain' ),
+	mapDomainAnalytics = analyticsMixin( 'mapDomain' );
 
+const DomainsStep = React.createClass( {
 	showGoogleApps: function() {
 		page( signupUtils.getStepUrl( this.props.flowName, this.props.stepName, 'google', this.props.locale ) );
 	},
@@ -57,6 +61,8 @@ module.exports = React.createClass( {
 			suggestion
 		};
 
+		registerDomainAnalytics.recordEvent( 'addDomainButtonClick', suggestion.domain_name, 'signup' );
+
 		if ( this.props.step.suggestion &&
 			this.props.step.suggestion.domain_name !== suggestion.domain_name ) {
 			// overwrite the Google Apps data if the user goes back and selects a different domain
@@ -87,7 +93,7 @@ module.exports = React.createClass( {
 
 	getThemeArgs: function() {
 		const themeSlug = this.getThemeSlug(),
-			themeSlugWithRepo = this.getThemeSlugWithRepo(),
+			themeSlugWithRepo = this.getThemeSlugWithRepo( themeSlug ),
 			themeItem = this.isPurchasingTheme()
 			? cartItems.themeItem( themeSlug, 'signup-with-theme' )
 			: undefined;
@@ -95,13 +101,12 @@ module.exports = React.createClass( {
 		return { themeSlug, themeSlugWithRepo, themeItem };
 	},
 
-	getThemeSlugWithRepo: function() {
-		const themeSlug = this.getThemeSlug();
+	getThemeSlugWithRepo: function( themeSlug ) {
 		if ( ! themeSlug ) {
 			return undefined;
 		}
-		// Only allow free themes for now; a valid theme value here (free or premium) will cause a theme_switch by Headstart.
-		return this.isPurchasingTheme() ? undefined : 'pub/' + themeSlug;
+		const repo = this.isPurchasingTheme() ? 'premium' : 'pub';
+		return `${repo}/${themeSlug}`;
 	},
 
 	submitWithDomain: function( googleAppsCartItem ) {
@@ -133,6 +138,8 @@ module.exports = React.createClass( {
 	handleAddMapping: function( sectionName, domain, state ) {
 		const domainItem = cartItems.domainMapping( { domain } );
 		const isPurchasingItem = true;
+
+		mapDomainAnalytics.recordEvent( 'addDomainButtonClick', domain, 'signup' );
 
 		SignupActions.submitSignupStep( Object.assign( {
 			processingMessage: this.translate( 'Adding your domain mapping' ),
@@ -173,8 +180,7 @@ module.exports = React.createClass( {
 
 	domainForm: function() {
 		const initialState = this.props.step ? this.props.step.domainForm : this.state.domainForm;
-		const isPlansOnlyTest = abtest( 'domainsWithPlansOnly' ) === 'plansOnly';
-		const isDeveloperFlow = 'developer' === this.props.flowName;
+
 		return (
 			<RegisterDomainStep
 				path={ this.props.path }
@@ -185,12 +191,12 @@ module.exports = React.createClass( {
 				mapDomainUrl={ this.getMapDomainUrl() }
 				onAddMapping={ this.handleAddMapping.bind( this, 'domainForm' ) }
 				onSave={ this.handleSave.bind( this, 'domainForm' ) }
-				offerMappingOption={ ! isDeveloperFlow }
+				offerMappingOption
 				analyticsSection="signup"
-				withPlansOnly={ isPlansOnlyTest }
+				domainsWithPlansOnly={ this.props.domainsWithPlansOnly }
 				includeWordPressDotCom
 				isSignupStep
-				showExampleSuggestions={ ! isDeveloperFlow }
+				showExampleSuggestions
 				suggestion={ this.props.queryObject ? this.props.queryObject.new : '' } />
 		);
 	},
@@ -201,14 +207,14 @@ module.exports = React.createClass( {
 
 		return (
 			<div className="domains-step__section-wrapper">
-				<MapDomain
+				<MapDomainStep
 					initialState={ initialState }
 					path={ this.props.path }
-					onAddDomain={ this.handleAddDomain }
-					onAddMapping={ this.handleAddMapping.bind( this, 'mappingForm' ) }
+					onRegisterDomain={ this.handleAddDomain }
+					onMapDomain={ this.handleAddMapping.bind( this, 'mappingForm' ) }
 					onSave={ this.handleSave.bind( this, 'mappingForm' ) }
-					productsList={ productsList }
-					withPlansOnly={ abtest( 'domainsWithPlansOnly' ) === 'plansOnly' }
+					products={ productsList.get() }
+					domainsWithPlansOnly={ this.props.domainsWithPlansOnly }
 					initialQuery={ initialQuery }
 					analyticsSection="signup" />
 			</div>
@@ -258,3 +264,10 @@ module.exports = React.createClass( {
 		);
 	}
 } );
+
+module.exports = connect( ( state ) => {
+	return {
+		// no user = DOMAINS_WITH_PLANS_ONLY
+		domainsWithPlansOnly: getCurrentUser( state ) ? currentUserHasFlag( state, DOMAINS_WITH_PLANS_ONLY ) : true
+	};
+} ) ( DomainsStep );
